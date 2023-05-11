ddcad3f55503a4ddf57b74a88084d029b2c18a31
felixDev22 <felixnzomo@gmail.com>
2023-05-11 16:46:11 +0300

    Add hotels controller rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'hotels/index'
+  get 'hotels/show'
+  get 'hotels/create'
+  get 'hotels/update'
+  get 'hotels/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
