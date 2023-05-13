66270b40b5a9232334bb57c67a7b00b93cff811a
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-13 15:33:16 +0300

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   namespace 'api' do
     namespace 'v1' do
-      resources :users
-      resources :hotels do
-        resources :rooms
-      end
-      resources :reservations 
+      resources :users do
+        resources :reservations 
+        resources :hotels do
+         resources :rooms
+       end
+      end 
     end
 end
 end
