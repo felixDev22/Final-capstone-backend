57ae9f760a92597aa9a4ce16a6449f26e498e810
Dmambo <mambotrader0@gmail.com>
2023-05-20 19:29:23 +0530

    Modified the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@ user2 = User.create(name: "ibrahima", email: "ibrahim1@gmail.com", password: "12
 user3 = User.create(name: "ibrahima1", email: "ibrahim2@gmail.com", password: "123456", password_confirmation: "123456")
 user4 = User.create(name: "ibrahima2", email: "ibrahim3@gmail.com", password: "123456", password_confirmation: "123456")
 
-hotel1 = Hotel.create(name: "Thorsten", photo: "https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", user_id: 1, location: "Nigeria", price: 450, rating: 8.5)
+hotel1 = Hotel.create(name: "Thorsten", photo: "https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", user_id: 1, location: "Nigeria", price: 450, rating: 8.5)
 hotel2 = Hotel.create(name: "Chan", photo: "https://images.pexels.com/photos/1458457/pexels-photo-1458457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", user_id: 2, location: "Ghana", price: 450, rating: 8)
 hotel3 = Hotel.create(name: "Pixabay", photo: "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", price: 350, location: "Guinea", rating: 7.5, user_id: 3)
 
@@ -23,6 +24,6 @@ room_type5 = RoomType.create(name: "queen")
 
 room1 = Room.create(room_type_id: 1, hotel_id: 1, price: 100, photo: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
 room2 = Room.create(room_type_id: 2, hotel_id: 1, price: 200, photo: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
-room3 = Room.create(room_type_id: 3, hotel_id: 1, price: 300, photo: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
-room4 = Room.create(room_type_id: 4, hotel_id: 1, price: 400, photo: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
-room5 = Room.create(room_type_id: 5, hotel_id: 1, price: 500, photo: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+room3 = Room.create(room_type_id: 3, hotel_id: 2, price: 300, photo: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+room4 = Room.create(room_type_id: 4, hotel_id: 2, price: 400, photo: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+room5 = Room.create(room_type_id: 5, hotel_id: 3, price: 500, photo: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
