fcff83c34aab890e9faab119490f6a2f7fed343a
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-23 17:58:27 +0300

    Fix validations attributes

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,17 +2,16 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :hotel
   validates :user, presence: true
-  validates :reservation_date, presence: true
-  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :number_of_persons, presence: true, numericality: { greater_than: 0 }
-  validates :departure_date, presence: true
+  validates :check_in, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :check_out, presence: true
   validate :departure_date_after_reservation_date
 
   def departure_date_after_reservation_date
-    return if departure_date.blank? || reservation_date.blank?
+    return if check_in.blank? || check_out.blank?
 
-    return unless departure_date < reservation_date
+    return unless check_out < check_in
 
-    errors.add(:departure_date, 'must be after the reservation date')
+    errors.add(:check_out, 'must be after the reservation date')
   end
 end
