2ba6be280c565c5c6a48a1c3e7736ab8b8bdc6ca
felixDev22 <felixnzomo@gmail.com>
2023-05-25 18:56:35 +0300

    Build deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,15 @@
+# Read more: https://github.com/cyu/rack-cors
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
     origins 'https://kefi-hotel-booking.netlify.app'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  allow do
+    origins 'http://127.0.0.1:3000'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
