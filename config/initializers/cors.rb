f0e1ccb6e16fe6a7cd883e8fedbbb0ee7da3701f
felixDev22 <felixnzomo@gmail.com>
2023-05-25 18:11:55 +0300

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -27,4 +27,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+   allow do
+    origins '*'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      
+  end
+end
+
 end
