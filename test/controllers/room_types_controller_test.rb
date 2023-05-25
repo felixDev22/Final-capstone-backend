e4c54458a66d05121c0b53a4c4b5e704fd04b037
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-05-25 03:46:17 +0300

    required changes

--- test/controllers/room_types_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RoomTypesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/users_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test 'should get create' do
-    get users_create_url
-    assert_response :success
-  end
-end
