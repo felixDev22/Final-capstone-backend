de382346ab7cfbfecc5521b15379bb1e11bdd3de
felixDev22 <felixnzomo@gmail.com>
2023-05-25 18:25:38 +0300

    Update cors3-allow all5

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
