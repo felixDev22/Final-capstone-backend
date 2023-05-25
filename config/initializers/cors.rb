dd4566fca8789f8579a75a0635bf78ad70a0de94
felixDev22 <felixnzomo@gmail.com>
2023-05-25 18:37:11 +0300

    Update cors3-allow all6

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins '*'
+    origins 'https://kefi-hotel-booking.netlify.app'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  allow do
+    origins 'https://kefi-hotel-booking-app.onrender.com/'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
