bc56e56a0ef77197ad5cbfc0012ba562b5a57afb
felixDev22 <felixnzomo@gmail.com>
2023-05-25 15:40:24 +0300

    Update render-build.sh file to db:seed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,13 +6,13 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
-  allow do
-    origins 'http://localhost:3000'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
+  # allow do
+  #   origins 'http://localhost:3000'
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #     credentials: true
+  # end
   allow do
     origins 'https://kefi-hotel-booking-app.onrender.com'
     resource '*',
