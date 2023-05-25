5fd0bd6b0ce8efaadc510e8ecb6658c66e850456
felixDev22 <felixnzomo@gmail.com>
2023-05-25 15:30:15 +0300

    Update cors.rb to fix load errors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_final_capstone_backend", secure: true, same_site: :strict
+Rails.application.config.session_store :cookie_store, key: "_final_capstone_backend", domain: "https://kefi-hotel-booking-app.onrender.com", secure: true, same_site: :strict
