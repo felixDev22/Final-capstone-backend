15812b47f1e12f66d259b85abb63f634ba51f572
felixDev22 <felixnzomo@gmail.com>
2023-06-24 16:36:58 +0300

    Remove user login restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :hotels, dependent: :destroy
   has_many :reservations, dependent: :destroy
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
